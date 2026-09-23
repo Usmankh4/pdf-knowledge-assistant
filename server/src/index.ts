ed6ae765b0d690740be47529fe2846206967b3bb
Usmankh4 <usmankhanx3@hotmail.com>
2026-09-22 23:01:27 -0400

    Query endpoint created

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,33 @@
 import express from "express";
-
+import cors from 'cors'
 const app = express();
 const PORT = 4000;
+
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
+app.use(express.json())
+
+app.get('/health', (request, response) => {
+  response.json({
+    status: "ok"
+  })
+})
+
+app.post('/query', (request, response) => {
+
+  const question = request.body.question;
+
+  response.json({
+    answer: `You asked: ${question}`
+  })
+
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
