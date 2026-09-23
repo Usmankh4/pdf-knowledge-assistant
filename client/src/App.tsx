c6ca81669a680db450f5915cb8d8bef65f00851c
Usmankh4 <usmankhanx3@hotmail.com>
2026-09-23 01:25:05 -0400

    add PDF upload flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, type FormEvent } from 'react'
 
 
 import './App.css'
@@ -6,11 +6,15 @@ import './App.css'
 function App() {
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [uploadedMessage, setUploadedMessage] = useState('');
+
+  const API_URL = 'http://localhost:4000';
 
 
   async function askQuestion(){
   try{
-    const response = await fetch('http://localhost:4000/query', {
+    const response = await fetch(`${API_URL}/query`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -28,19 +32,58 @@ function App() {
   }
 }
 
+async function uploadPdf(event: FormEvent){
+  event.preventDefault();
+  if(!selectedFile){
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append('pdf', selectedFile);
+
+  const response = await fetch(`${API_URL}/upload`, {
+    method: "POST",
+    body: formData
+    })
+  const data = await response.json();
+  setUploadedMessage(data.message);
+ 
+
+}
+
+console.log(selectedFile);
+
 
   return (
     <div>
 
-      <input
-      type='text'
+
+      <input type='text' 
       value={question}
       onChange={(e) => setQuestion(e.target.value)}
-      ></input>
+      />
       <button onClick={askQuestion}>Ask Question</button>
 
+      <form onSubmit={uploadPdf}>
+      <input
+      type='file'
+      accept='application/pdf'
+      onChange={(event) => {
+        const file = event.target.files?.[0] ?? null;
+        setSelectedFile(file)
+      }} 
+      />
+      <button type='submit'>Upload PDF</button>
+      </form>
 
+      
 
+     
+     
+      
+
+      <p>{selectedFile?.name}</p>
+      <p>{uploadedMessage}</p>
       <p>{answer}</p>
 
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,11 @@ import express from "express";
 import cors from 'cors'
 const app = express();
 const PORT = 4000;
+import multer from "multer";
 
+const upload = multer({
+  dest: "uploads/",
+})
 
 app.use(cors({
   origin: "http://localhost:5173"
@@ -24,6 +28,16 @@ app.post('/query', (request, response) => {
   })
 
 })
+
+app.post('/upload', upload.single("pdf"), (request, response) => {
+
+  console.log(request.file);
+  response.json({
+    message: "PDF uploaded",
+  })
+})
+
+
 
 
 app.listen(PORT, () => {
